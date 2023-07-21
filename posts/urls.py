aca16dca80a0db6a8d94e7c2c72b92a967f2113b
sue5520607@skuniv.ac.kr <sue5520607@skuniv.ac.kr>
2023-07-21 15:55:01 +0900

    오늘 치 공부

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from .views import post_list_view, post_create_view, post_update_view,post_detai
 app_name ='posts'
 urlpatterns =[
     path('', post_list_view, name='post-list'),
-    path('new/', post_create_view),
+    path('new/', post_create_view, name = 'post-create'),
     path('<int:id>/edit/',post_update_view ),
     path('<int:id>/', post_detail_view),
     path('<int:id>/delete/',post_confirm_delete_view),
